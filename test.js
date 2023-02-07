e67aead939df8a5146439f25ee66f16a999ae883
Abhijit Mali <maliabhijit183@gmail.com>
2023-02-07 12:48:13 +0530

    initial commit

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-Helllo 
+const express = require('express')
+const app = express()
+const port = 500
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
